958bf49649374b682388f73c20db3416e53e79af
Josephine Fahy <4753015+earthwytch@users.noreply.github.com>
2023-03-09 16:45:41 +1300

    Update EasyReadParagraph.php

--- src/models/EasyReadParagraph.php
+++ src/models/EasyReadParagraph.php
@@ -6,15 +6,8 @@ use SilverStripe\Security\Permission;
 
 class EasyReadParagraph extends EasyReadLine
 {
-<<<<<<< HEAD
-<<<<<<< HEAD
-    private static $table_name = 'EasyReadHeading';
-=======
+
     private static $table_name = 'EasyReadParagraph';
->>>>>>> 0e2e4d7... Adding tests and package requirements
-=======
-    private static $table_name = 'EasyReadParagraph';
->>>>>>> 4602a6002b68228e35437259a171126851c9ef4e
     private static $line_type = 'Paragraph';
 
     public function canView($member = null)
