d5249d79bf9442cabb86a7e58be017f06629ab1c
Josephine Fahy <4753015+earthwytch@users.noreply.github.com>
2023-03-09 16:46:22 +1300

    Update EasyReadList.php

--- src/models/EasyReadList.php
+++ src/models/EasyReadList.php
@@ -4,24 +4,10 @@ namespace Earthwytch\SilverStripeEasyRead;
 
 use SilverStripe\Security\Permission;
 
-<<<<<<< HEAD
-<<<<<<< HEAD
 class EasyReadDefinition extends EasyReadLine
 {
-    private static $table_name = 'EasyReadHeading';
-    private static $line_type = 'Paragraph';
-=======
-class EasyReadList extends EasyReadLine
-{
     private static $table_name = 'EasyReadList';
     private static $line_type = 'List';
->>>>>>> 0e2e4d7... Adding tests and package requirements
-=======
-class EasyReadList extends EasyReadLine
-{
-    private static $table_name = 'EasyReadList';
-    private static $line_type = 'List';
->>>>>>> 4602a6002b68228e35437259a171126851c9ef4e
 
     public function canView($member = null)
     {
